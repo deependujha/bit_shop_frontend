952fda762e1f34e8226ed3f39412d3f982eb38c2
Deependu_Jha <deependujha21@gmail.com>
2022-07-18 00:22:03 +0530

    showing msg in myOrder section instead of redirecting to create account page.

--- pages/myOrder.js
+++ pages/myOrder.js
@@ -5,7 +5,8 @@ import MyOrder from "../components/MyOrder";
 const myOrder = ({ usrAddr, usrDetails }) => {
   const router = useRouter();
   if (usrDetails.name==undefined) {
-    router.push("/myAccount");
+    return <div className="container my-5">
+    <h3>Please login first.</h3></div>
   } else {
     return <MyOrder usrAddr={usrAddr} usrDetails={usrDetails}/>;
   }
