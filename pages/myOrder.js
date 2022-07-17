42ac344e58e3be528bdeb10b487987e19dc83009
Deependu_Jha <deependujha21@gmail.com>
2022-07-17 16:34:28 +0530

    my order page is redirecting in case user is not signed in

--- pages/myOrder.js
+++ pages/myOrder.js
@@ -1,8 +1,14 @@
 import React from "react";
+import { useRouter } from "next/router";
 import MyOrder from "../components/MyOrder";
 
-const myOrder = () => {
-  return <MyOrder />;
+const myOrder = ({ usrAddr }) => {
+  const router = useRouter();
+  if (usrAddr == "") {
+    router.push("/myAccount");
+  } else {
+    return <MyOrder />;
+  }
 };
 
 export default myOrder;
